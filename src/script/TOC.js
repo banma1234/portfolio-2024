85c444b2da67cf93f1b05b545b26fe821276c015
banma1234 <banma1234@gmail.com>
2024-02-19 18:26:03 +0900

    refactor: slide 크기변화에 따라 scroll point 변경

--- src/script/TOC.js
+++ src/script/TOC.js
@@ -1,22 +1,43 @@
 const TOC = document.getElementById("TOC");
 const tocItems = document.querySelectorAll(".TOC_items");
 const slides = document.querySelectorAll(
-  "#slide0, #slide1, #slide2, #slide3, #slide4, #slide5"
+  "#slide0, #slide1, #slide2, #slide3, #slide4, #slide5",
 );
 
 tocItems.forEach((item, index) => (item.dataset.index = index));
 slides.forEach((slide, index) => (slide.dataset.index = index));
 
-TOC.addEventListener("click", (e) => {
+TOC.addEventListener("click", e => {
   if (e.target.matches(".TOC_items")) {
-    console.log(slides[e.target.dataset.index]);
+    const index = +e.target.dataset.index;
+    switch (index) {
+      case 0:
+        console.log("000");
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        break;
+      case 1:
+        window.scrollTo({ top: 1100, left: 0, behavior: "smooth" });
+        break;
+      default:
+        window.scrollTo({
+          top: 3600 + 1400 * (index - 2),
+          left: 0,
+          behavior: "smooth",
+        });
+    }
+    // index === 1
+    //   ? window.scrollTo({ top: 1100, left: 0, behavior: "smooth" })
+    //   : window.scrollTo({ top: 1400 * index, left: 0, behavior: "smooth" });
   }
 });
 
+const a = [0, 1100, 3600, 5000, 6400, 7800];
+const w = [0, 1100, 2400, 3800, 5200, 6600];
+
 let selectedIndex = 0;
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
+  entries => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         tocItems[selectedIndex].classList.remove("selected");
         selectedIndex = entry.target.dataset.index;
@@ -28,7 +49,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  { threshold: 0.8 }
+  { threshold: 0.8 },
 );
 
-slides.forEach((slide) => observer.observe(slide));
+slides.forEach(slide => observer.observe(slide));
