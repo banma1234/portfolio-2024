a81c23d4048b67cc2f2fda157c1a2b638da08771
banma1234 <banma1234@gmail.com>
2024-02-24 19:17:29 +0900

    fix: slide1 좌표값 수정

--- src/script/TOC.js
+++ src/script/TOC.js
@@ -1,13 +1,13 @@
 const TOC = document.getElementById("TOC");
 const tocItems = document.querySelectorAll(".TOC_items");
 const slides = document.querySelectorAll(
-  "#slide0, #slide1, #slide2, #slide3, #slide4, #slide5"
+  "#slide0, #slide1, #slide2, #slide3, #slide4, #slide5",
 );
 
 tocItems.forEach((item, index) => (item.dataset.index = index));
 slides.forEach((slide, index) => (slide.dataset.index = index));
 
-TOC.addEventListener("click", (e) => {
+TOC.addEventListener("click", e => {
   if (e.target.matches(".TOC_items")) {
     const index = +e.target.dataset.index;
     switch (index) {
@@ -16,7 +16,7 @@ TOC.addEventListener("click", (e) => {
         window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
         break;
       case 1:
-        window.scrollTo({ top: 1100, left: 0, behavior: "smooth" });
+        window.scrollTo({ top: 1200, left: 0, behavior: "smooth" });
         break;
       default:
         window.scrollTo({
@@ -30,8 +30,8 @@ TOC.addEventListener("click", (e) => {
 
 let selectedIndex = 0;
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
+  entries => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         tocItems[selectedIndex].classList.remove("selected");
         selectedIndex = entry.target.dataset.index;
@@ -43,7 +43,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  { threshold: 0.8 }
+  { threshold: 0.8 },
 );
 
-slides.forEach((slide) => observer.observe(slide));
+slides.forEach(slide => observer.observe(slide));
