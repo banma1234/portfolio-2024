988c8f22cae82f7efa6e2248c7a58dbefef0d0a1
banma1234 <banma1234@gmail.com>
2024-02-25 19:30:30 +0900

    feat: 모달창 작성 및 디자인 수정

--- src/script/global.js
+++ src/script/global.js
@@ -3,11 +3,31 @@ const imagChange = document.querySelectorAll(".img_change");
 const imageMap = ["image/profile.png", "image/profile_2.jpg"];
 
 imagChange.forEach((item, i) => {
-  item.addEventListener("mouseover", (e) => {
+  item.addEventListener("mouseover", e => {
     image.classList.add("fade_out");
     setTimeout(() => {
       image.src = imageMap[i];
       image.classList.remove("fade_out");
     }, 300);
   });
+});
+
+const body = document.getElementsByTagName("body")[0];
+const carousel = document.querySelectorAll(".icon-cards__item > img");
+const modal = document.getElementById("modal");
+const modalBody = document.querySelector("#modal_body > img");
+const modalClose = document.getElementById("modal_close");
+
+carousel.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.classList.add("activate");
+    modalBody.src = img.src;
+    body.classList.add("scrollLock");
+  });
+});
+
+modalClose.addEventListener("click", () => {
+  modalBody.src = "";
+  modal.classList.remove("activate");
+  body.classList.remove("scrollLock");
 });
