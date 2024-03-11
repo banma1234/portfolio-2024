57afa5c592af7fcc5671365aa411f2084f216c3e
banma1234 <banma1234@gmail.com>
2024-03-11 18:52:52 +0900

    fix: 페이지 스크롤좌표값 수정

--- src/script/TOC.js
+++ src/script/TOC.js
@@ -1,13 +1,13 @@
 const TOC = document.getElementById("TOC");
 const tocItems = document.querySelectorAll(".TOC_items");
 const slides = document.querySelectorAll(
-  "#slide0, #slide5, #slide1, #slide2, #slide3, #slide4"
+  "#slide0, #slide5, #slide1, #slide2, #slide3, #slide4",
 );
 
 tocItems.forEach((item, index) => (item.dataset.index = index));
 slides.forEach((slide, index) => (slide.dataset.index = index));
 
-TOC.addEventListener("click", (e) => {
+TOC.addEventListener("click", e => {
   if (e.target.matches(".TOC_items")) {
     const index = +e.target.dataset.index;
     switch (index) {
@@ -15,14 +15,14 @@ TOC.addEventListener("click", (e) => {
         window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
         break;
       case 1:
-        window.scrollTo({ top: 1100, left: 0, behavior: "smooth" });
+        window.scrollTo({ top: 1200, left: 0, behavior: "smooth" });
         break;
       case 2:
         window.scrollTo({ top: 2650, left: 0, behavior: "smooth" });
         break;
       default:
         window.scrollTo({
-          top: 5600 + 1400 * (index - 3),
+          top: 5600 + 1200 * (index - 3),
           left: 0,
           behavior: "smooth",
         });
@@ -32,8 +32,8 @@ TOC.addEventListener("click", (e) => {
 
 let selectedIndex = 0;
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
+  entries => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         tocItems[selectedIndex].classList.remove("selected");
         selectedIndex = entry.target.dataset.index;
@@ -45,7 +45,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  { threshold: 0.8 }
+  { threshold: 0.8 },
 );
 
-slides.forEach((slide) => observer.observe(slide));
+slides.forEach(slide => observer.observe(slide));
