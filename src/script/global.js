d78452c2b5498dbdbeed760488f087373f5fbd7b
chocoham <banma1234@gmail.com>
2024-09-30 02:18:30 +0900

    refactor: contact 완, opensource 작성중

--- src/script/global.js
+++ src/script/global.js
@@ -4,7 +4,7 @@ const imagChange = document.querySelectorAll(".img_change");
 const imageMap = ["image/profile.png", "image/profile_2.jpg"];
 
 imagChange.forEach((item, i) => {
-  item.addEventListener("mouseover", e => {
+  item.addEventListener("mouseover", (e) => {
     image.classList.add("fade_out");
     setTimeout(() => {
       image.src = imageMap[i];
@@ -17,11 +17,12 @@ imagChange.forEach((item, i) => {
 // #region 모달창 제어
 const body = document.getElementsByTagName("body")[0];
 const carousel = document.querySelectorAll(".icon-cards__item > img");
+const profile = document.getElementById("contact__overlay");
 const modal = document.querySelector(".modal");
 const modalBody = document.querySelector("#modal_body > img");
 const modalClose = document.getElementById("modal_close");
 
-carousel.forEach(img => {
+carousel.forEach((img) => {
   img.addEventListener("click", () => {
     body.classList.add("scrollLock");
     modal.classList.add("activate");
@@ -29,6 +30,14 @@ carousel.forEach(img => {
       modalBody.src = img.src;
     }, 600);
   });
+});
+
+profile.addEventListener("click", () => {
+  body.classList.add("scrollLock");
+  modal.classList.add("activate");
+  setTimeout(() => {
+    modalBody.src = "image/doTest_2.jpg";
+  }, 600);
 });
 
 modalClose.addEventListener("click", () => {
@@ -40,7 +49,7 @@ modalClose.addEventListener("click", () => {
 
 // #region 스크롤에 따른 컴포넌트 제어
 const components = document.querySelectorAll(
-  "#canvas, #contact, .blog, .portfolio, #post-container, #opensource_1, #opensource_2, #opensource_3",
+  "#canvas, #contact, .blog, .portfolio, #post-container, #opensource_1, #opensource_2, #opensource_3"
 );
 
 /**
@@ -71,18 +80,18 @@ export function scrollAction(rotationX) {
 //#endregion
 
 // #region 2D html 컴포넌트에 3D 애니메이션 적용
-const contact = document.getElementById("contact");
-const overlay = document.getElementById("contact_overlay");
+const contact = document.getElementById("contact__profile__container");
+const overlay = document.getElementById("contact__overlay");
 
-overlay.addEventListener("mousemove", e => {
+overlay.addEventListener("mousemove", (e) => {
   let rotateX = (1 / 10) * e.offsetY - 20;
-  let rotateY = (-1 / 20) * e.offsetX + 20;
+  let rotateY = (-1 / 10) * e.offsetX + 20;
 
-  contact.style.transform = `perspective(800px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  contact.style.transform = `perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
 
 overlay.addEventListener("mouseenter", () => {
-  contact.style.transition = `transform 0.15s`;
+  contact.style.transition = `transform 0.1s`;
 });
 
 overlay.addEventListener("mouseleave", () => {
